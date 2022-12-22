e53ae0adea41d97618731672007e718ce5038f31
Selin Monroe <78521426+codematters@users.noreply.github.com>
2022-12-22 15:54:04 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburgerMenu = document.querySelector("#hamburger")
-const menu = document.querySelector("#menu")
-
-hamburgerMenu.addEventListener("click", ()=> {
-    menu.classList.toggle("hidden")
-    hamburger.classList.toggle("bg-white")
-})

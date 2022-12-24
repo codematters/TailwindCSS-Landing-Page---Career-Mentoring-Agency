5eac2ed37f5bf7c0ae2237d55b129eecfb3f4b77
Selin Monroe <78521426+codematters@users.noreply.github.com>
2022-12-23 22:19:45 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "color-blue": "#39A9DB",
+        "color-lightblue": "#EBF6FB",
+        "color-orange": "#F39237",
+        "color-lightorange": "#f7ba81",
+        "color-mediumorange": "#e1740e",
+        "color-brownorange": "#d16c0d",
+        "color-darkorange": "#b25200",
+        "color-darkblue": "#242A45",
+        "color-grey": "#9194A2",
+        "color-mediumgrey": "#E8E8E8",
+        "color-white": "#f7f7f7",
+        "color-darkwhite": "#d6d6d6",
+        "color-mediumblue": "#164357",
+        "color-lightblue": "#D7EDF7"
+      },
+
+     container: {
+        center: true,
+        padding: "1rem",
+        screens : {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px"
+        }
+      },
+    },
+    fontFamily:{
+      poppins:['Poppins', 'sans-serif']
+    },
+  },
+  plugins: [],
+};
